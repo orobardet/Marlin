ca2e5885ce51ce5fea93485246147c80461d9a37
Olivier Robardet <olivier.robardet@gmail.com>
2017-08-21 07:59:50 +0200

    Renren3D configuration update

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -561,7 +561,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { XYZ_STEPS, XYZ_STEPS, XYZ_STEPS, 150.5 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { XYZ_STEPS, XYZ_STEPS, XYZ_STEPS, 150.50 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -576,7 +576,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 9000, 9000, 9000, 9000 }
+#define DEFAULT_MAX_ACCELERATION      { 999, 999, 999, 999 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -586,9 +586,9 @@
  *   M204 R    Retract Acceleration
  *   M204 T    Travel Acceleration
  */
-#define DEFAULT_ACCELERATION          3000    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION          999    // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  999    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   999    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
  * Default Jerk (mm/s)
